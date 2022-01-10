635e9be3c185e4b7e8c3d647a8905de5df2f6598
esra-ozturk <esrakeklik24@gmail.com>
2022-01-10 09:37:59 +0100

    method transfered to new module

--- status_state_api/app.py
+++ status_state_api/app.py
@@ -1,20 +1,11 @@
 from flask import  Flask,jsonify
-import docker
+import container
 
 app = Flask(__name__)
-
-def create_container_list() :
-    status_state_list = []
-    client = docker.from_env()
-    for container in client.containers.list():
-        container_dict = {'container_name':container.name,'image':container.attrs['Config']['Image'],'id':container.id,'status':container.status}
-        status_state_list.append(container_dict)
-        
-    return  status_state_list
     
 @app.route("/status_state_api", methods=['GET'])
 def main():
-    result = create_container_list()
+    result = container.create_container_list()
    
     return jsonify(result)
    
