e3f4418ca4c7ce748c51c27d281f85ca55643986
esra-ozturk <esrakeklik24@gmail.com>
2022-01-10 14:52:48 +0100

    folder rearranged

--- status_state_api/container.py
+++
@@ -1,9 +0,0 @@
-import docker
-def create_container_list() :
-    status_state_list = []
-    client = docker.from_env()
-    for container in client.containers.list():
-        container_dict = {'container_name':container.name,'image':container.attrs['Config']['Image'],'id':container.id,'status':container.status}
-        status_state_list.append(container_dict)
-        
-    return  status_state_list
